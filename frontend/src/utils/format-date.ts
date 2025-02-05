ffe32cba982b6eff288c917e064649b2264d4b75
1uciuszzz <nick_zzhao@outlook.com>
2025-02-05 08:47:37 +0800

    fix:时间格式化时,未设置时区导致错误的时间显示

--- frontend/src/utils/format-date.ts
+++ frontend/src/utils/format-date.ts
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
+import { zhCN } from "date-fns/locale";
 
 export const formatDate = (date: string) => {
-  return format(date, `yyyy/MM/dd hh:mm:ss`);
+  return format(date, `yyyy/MM/dd hh:mm:ss`, { locale: zhCN });
 };
